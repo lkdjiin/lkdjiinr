d57384af46270223b7b3acfb2d722887325fb03e
lkdjiin <xavier.nayrac@gmail.com>
2015-07-26 14:43:42 +0200

    Misc utility functions

---
+++ R/misc.r
@@ -0,0 +1,41 @@
+#' Alias to Sys.time
+#'
+#' @examples
+#' now()
+#' #=> "2015-07-26 14:23:25 CEST"
+#' @seealso \code{\link{Sys.time}}
+now <- Sys.time
+
+#' Recall last computed value.
+#'
+#' Useful function to working interactively. It's a king of shorter
+#' alias for \code{.Last.value}.
+#'
+#' @examples
+#' 1 + 1
+#' #=> 2
+#' .()
+#' #=> 2
+#' rnorm(3)
+#' #=> 1.3475191 -0.2122325 -1.4027707
+#' .()
+#' #=> 1.3475191 -0.2122325 -1.4027707
+. <- function() .Last.value
+
+#' Alias to setwd.
+#'
+#' @examples
+#' cd('foo')
+#' getwd()
+#' #=> "foo"
+#' @seealso \code{\link{setwd}}
+cd <- setwd
+
+#' Change Directory Dot Dot.
+#'
+#' Move up one directory level. Same as UNIX `cd ..`.
+#'
+#' @examples
+#' cddd()
+cddd <- setwd('..')
+
