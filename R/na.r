f389b276d79764ef24b37f9e25c2589087787ec2
lkdjiin <xavier.nayrac@gmail.com>
2015-07-26 10:23:43 +0200

    Some function related to NA values

---
+++ R/na.r
@@ -0,0 +1,40 @@
+#' Count number of NAs in a vector.
+#'
+#' @param x A vector.
+#' @return The number of NAs in the vector x.
+#' @examples
+#' x <- c(NA, 'a', 'b', NA, 'c')
+#' na_count(x)
+#' # 2
+#' @seealso \code{\link{nas}}
+na_count <- function(x) sum(is.na(x))
+
+#' Alias for \code{na_count}
+#' @seealso \code{\link{na_count}}
+nas <- function(x) na_count(x)
+
+#' Proportion of NAs in a vector.
+#'
+#' What proportion of NA values are in a vector?
+#'
+#' @param x A vector.
+#' @return A number between 0 (no NAs) and 1 (all values are NAs).
+#' @examples
+#' x <- c(NA, 'a', 'b', NA, 'c')
+#' na_prop(x)
+#' # 0.4
+#' @seealso \code{\link{na_percent}}
+na_prop <- function(x) na_count(x) / length(x)
+
+#' Percentage of NAs in a vector.
+#'
+#' What percentage of NA values are in a vector?
+#'
+#' @param x A vector.
+#' @return A number between 0 (no NAs) and 100 (all values are NAs).
+#' @examples
+#' x <- c(NA, 'a', 'b', NA, 'c')
+#' na_prop(x)
+#' # 40
+#' @seealso \code{\link{na_prop}}
+na_percent <- function(x) na_prop(x) * 100
---
+++ README.md
@@ -0,0 +1,19 @@
+# R Utility Functions
+
+Some of my personal R utility functions in a package.
+
+This is mostly a pretext to make my first R package ;)
+
+## Install
+
+    install_github('lkdjiinr', 'lkdjiin')
+
+## Usage
+
+    library(lkdjiinr)
+
+    library(help=lkdjiinr)
+
+## License
+
+MIT
