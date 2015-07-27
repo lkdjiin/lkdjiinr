56aa0c1e85a508f8f8e93fbfab35624c85dbfb83
lkdjiin <xavier.nayrac@gmail.com>
2015-07-27 14:51:14 +0200

    Remove operator %+%

--- R/string.r
+++ R/string.r
@@ -1,17 +1,3 @@
-#' String Concatenation Operator.
-#'
-#' The world is in need of an operator to concatenate strings. This will
-#' be %+%.
-#'
-#' @return A string.
-#' @examples
-#' "foo" %+% "bar"
-#' #=> "foobar"
-#'
-#' c("a", "a") %+% c("b", "c")
-#' #=> "ab" "ac"
-"%+%" <- function(...) paste(..., sep='')
-
 #' Alias to chartr.
 #'
 #' Like \code{chartr} but with a different order of arguments.
